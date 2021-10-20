b56ef90cf6d640634af11f48dfd43be7fae4f10b
OGZATECH-ALBATR\okivrak <oguzhan.kivrak@ogzatech.com>
2021-10-20 21:34:12 +0300

    login

---
+++ StayYonetimProg/WebFrmTest.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebFrmTest.aspx.cs" Inherits="StayYonetimProg.WebFrmTest" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:CheckBoxList ID="chkKirsatiyeListesi" runat="server" 
+                AutoPostBack ="True"
+                OnSelectedIndexChanged="chkKirsatiyeListesi_SelectedIndexChanged">
+            </asp:CheckBoxList>
+            <br />
+            <asp:Label ID="lblBilgi" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ StayYonetimProg/WebFrmTest.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StayYonetimProg
+{
+    public partial class WebFrmTest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                List<veri> chkListDataKaynagi = new List<veri>();
+                for (int i = 0; i < 5; i++)
+                {
+                    veri item = new veri();
+                    item.code = i + "code";
+                    item.name = i + " adım ";
+
+                    chkListDataKaynagi.Add(item);
+                }
+
+                chkKirsatiyeListesi.DataTextField = "name";
+                chkKirsatiyeListesi.DataValueField = "code";
+                chkKirsatiyeListesi.DataSource = chkListDataKaynagi;
+                chkKirsatiyeListesi.DataBind();
+            }
+        }
+
+        protected void chkKirsatiyeListesi_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblBilgi.Text = "";
+            for(int i = 0; i < chkKirsatiyeListesi.Items.Count; i++)
+            {
+                if (chkKirsatiyeListesi.Items[i].Selected)
+                    lblBilgi.Text = lblBilgi.Text + " - " + chkKirsatiyeListesi.Items[i].Value + " :" + chkKirsatiyeListesi.Items[i].Text;
+            }
+        }
+
+        public class veri
+        {
+            public string name { set; get; }
+            public string code { set; get; }
+        }
+    }
+}
